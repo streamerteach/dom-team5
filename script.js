4bad5d662f75e7ea74d09ce9af3828e7b9f96c20
Subatomic0 <saunawithabeer@proton.me>
2024-08-29 17:26:57 +0300

    ugly but functional border on hover

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 window.onload = function() {
     // Lyssna efter händelser
+    const elements = document.querySelectorAll('.team-member');
+    elements.forEach(elem => elem.addEventListener('mouseenter', evt => {
+        evt.target.style.border = 'solid';
+    }));
+    elements.forEach(elem => elem.addEventListener('mouseleave', evt => {
+        evt.target.style.border = 'none';
+    }));
 }
 var editTitle = function(e) {
     // Ändra "team-title"
@@ -7,6 +14,7 @@ var editTitle = function(e) {
 
 function effectOn(id){
     // Rita en ram runt personen, kanske lite drop shadow?
+
 }
 
 function effectOff(id){
